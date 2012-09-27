e54b29d3c5344ebb881197016f8beb2718f46178
Frank Sagurna <fgunni@01019freenet.de>
2012-09-27 15:02:52 +0300

    Well, not needed anywhere
    As i am on this file: categories2 is not used in entire sugar (only occurences are in this file). Is not used can be deleted.

--- modules/ACLRoles/DetailView.php
+++ modules/ACLRoles/DetailView.php
@@ -67,20 +67,10 @@ $names = ACLAction::setupCategoriesMatrix($categories);
 // Sort by translated categories
 uksort($categories,"langCompare");
 
-$categories2 = array();
-$categories2=$categories;
-$hidden_categories = array(
-"KBDocuments", "Campaigns","Forecasts","ForecastSchedule",
-"Emails","EmailTemplates","EmailMarketing","Reports");
-foreach($hidden_categories as $v){
-	if (isset($categories2[$v])) {
-	   unset($categories2[$v]);
-	}
-}
 if(!empty($names))$tdwidth = 100 / sizeof($names);
 $sugar_smarty->assign('ROLE', $role->toArray());
 $sugar_smarty->assign('CATEGORIES', $categories);
-$sugar_smarty->assign('CATEGORIES2', $categories2);
+
 $sugar_smarty->assign('TDWIDTH', $tdwidth);
 $sugar_smarty->assign('ACTION_NAMES', $names);
 
